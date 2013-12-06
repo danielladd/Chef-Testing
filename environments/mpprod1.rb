4a0df951353e673fb5a4e20f7ac82c5493a08557
Matt Hurne <mhurne@commercehub.com>
2013-12-05 20:33:17 -0500

    Update configuration of Klerk nodes in the mpprod1 environment

--- environments/mpprod1.rb
+++ environments/mpprod1.rb
@@ -2,9 +2,9 @@ name "mpprod1"
 description "marketplace production environment 1"
 default_attributes "chub-klerk" => {
     "mongo_addresses" => [ "mdb01:27017", "mdb02:27017", "mdb03:27017" ],
-    "quartz_database_url" => "jdbc:jtds:sqlserver://sqlintv8/Klerk_Quartz;user=Klerk;password=Kusr01!",
+    "quartz_database_url" => "jdbc:jtds:sqlserver://sqlps/Klerk_Quartz;user=Klerk_Admin;password=KAdminuser01!",
     "hazelcast_group_name" => "mpprod1_klerk",
-    "hazelcast_group_password" => "mpprod1_klerk",
+    "hazelcast_group_password" => "dbYsD3PVD9HEpGbQ",
     "antivirus_hosts" => [ "vs01", "vs02" ],
     "event_notification_endpoints" => {
         "productstream" => "http://api.buyspace.com/klerk/productDataEvents"
