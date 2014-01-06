b465f9e6ffae17661a222e64d3c34034f0af3210
Matt Hurne <mhurne@commercehub.com>
2014-01-06 16:21:23 -0500

    CPE-1354 Update klerk Quartz database connection URL for the mpdemo1 environment

--- environments/mpdemo1.rb
+++ environments/mpdemo1.rb
@@ -2,7 +2,7 @@ name "mpdemo1"
 description "marketplace demo environment 1"
 default_attributes "chub-klerk" => {
     "mongo_addresses" => [ "new.commercehub.com:27017" ],
-    "quartz_database_url" => "jdbc:jtds:sqlserver://sqlvm81/mpdemo1_klerk_quartz;user=devusr01;password=usrdev",
+    "quartz_database_url" => "jdbc:jtds:sqlserver://sqlps/MPDemo1_Klerk_Quartz;user=Mpdemo1_Klerk_Admin;password=Adminuser01_MPDemo!",
     "hazelcast_group_name" => "mpdemo1_klerk",
     "hazelcast_group_password" => "NwicZ8LHuXoaUHV4",
     "antivirus_hosts" => [ "qa-scanengine.commercehub.com" ],
