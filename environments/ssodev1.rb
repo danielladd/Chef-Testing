73e83e9c319e3cd4c5985431cdf74808a32b748c
David M. Carr <dcarr@commercehub.com>
2014-01-28 09:17:24 -0500

    add vault config to sso environments

--- environments/ssodev1.rb
+++ environments/ssodev1.rb
@@ -1,6 +1,11 @@
 name "ssodev1"
 description "SSO development environment 1"
 default_attributes(
+    "chub-vault" => {
+      "app_url" => "file:///var/vault/staged-vault.jar",
+      "ldap_read_host" => "ssodev1ldap1.nexus.commercehub.com",
+      "search_password" => "search"
+    },
     "chub-castle" => {
       "vault_url" => "https://ssodev1-vault.nexus.commercehub.com:8443",
       "truststore_file" => "dev-truststore.jks",
