795d5fa66c3ec394a1b62969f9610a756618f1a0
David M. Carr <dcarr@commercehub.com>
2014-01-29 08:52:18 -0500

    pin cookbook versions in ssoqa1 environment

--- environments/ssodev1.rb
+++ environments/ssodev1.rb
@@ -1,21 +1,21 @@
 name "ssodev1"
 description "SSO development environment 1"
 default_attributes(
-    "chub-vault" => {
-      "app_url" => "file:///var/vault/staged-vault.jar",
-      "ldap_read_host" => "ssodev1-ldap-read.nexus.commercehub.com",
-      "search_password" => "search"
-    },
-    "chub-castle" => {
-      "app_url" => "file:///var/castle/staged-castle.war",
-      "vault_url" => "https://ssodev1-vault.nexus.commercehub.com:8443",
-      "truststore_file" => "dev-truststore.jks",
-      "keystore_file" => "dev-keystore.jks"
-    },
-    "chub-plaza" => {
-      "app_url" => "file:///var/plaza/staged-plaza.jar",
-      "cas_server_url" => "https://ssodev1castle1.nexus.commercehub.com:8443",
-      "cas_service_url" => "http://ssodev1plaza1.nexus.commercehub.com:8080/shiro-cas",
-      "graphite_host" => "stat02.nexus.commercehub.com"
-    }
+  "chub-vault" => {
+    "app_url" => "file:///var/vault/staged-vault.jar",
+    "ldap_read_host" => "ssodev1-ldap-read.nexus.commercehub.com",
+    "search_password" => "search"
+  },
+  "chub-castle" => {
+    "app_url" => "file:///var/castle/staged-castle.war",
+    "vault_url" => "https://ssodev1-vault.nexus.commercehub.com:8443",
+    "truststore_file" => "dev-truststore.jks",
+    "keystore_file" => "dev-keystore.jks"
+  },
+  "chub-plaza" => {
+    "app_url" => "file:///var/plaza/staged-plaza.jar",
+    "cas_server_url" => "https://ssodev1castle1.nexus.commercehub.com:8443",
+    "cas_service_url" => "http://ssodev1plaza1.nexus.commercehub.com:8080/shiro-cas",
+    "graphite_host" => "stat02.nexus.commercehub.com"
+  }
 )
--- environments/ssoqa1.rb
+++ environments/ssoqa1.rb
@@ -1,21 +1,28 @@
 name "ssoqa1"
 description "SSO QA environment 1"
+cookbook_versions({
+  "chub-vault" => "= 0.1.1",
+  "chub-castle" => "= 0.1.9",
+  "chub-plaza" => "= 0.1.0",
+})
+# TODO: switch to this when we next update vault
+# "ldap_read_host" => "ssoqa1-ldap-read.nexus.commercehub.com",
 default_attributes(
-    "chub-vault" => {
-      "app_url" => "file:///var/vault/staged-vault.jar",
-      "ldap_read_host" => "ssoqa1-ldap-read.nexus.commercehub.com",
-      "search_password" => "search"
-    },
-    "chub-castle" => {
-      "app_url" => "file:///var/castle/staged-castle.war",
-      "vault_url" => "https://ssoqa1-vault.nexus.commercehub.com:8443",
-      "truststore_file" => "dev-truststore.jks",
-      "keystore_file" => "dev-keystore.jks"
-    },
-    "chub-plaza" => {
-      "app_url" => "file:///var/plaza/staged-plaza.jar",
-      "cas_server_url" => "https://ssoqa1castle1.nexus.commercehub.com:8443",
-      "cas_service_url" => "http://ssoqa1plaza1.nexus.commercehub.com:8080/shiro-cas",
-      "graphite_host" => "stat02.nexus.commercehub.com"
-    }
+  "chub-vault" => {
+    "app_url" => "file:///var/vault/staged-vault.jar",
+    "ldap_read_host" => "ssoqa1ldap1.nexus.commercehub.com",
+    "search_password" => "search"
+  },
+  "chub-castle" => {
+    "app_url" => "file:///var/castle/staged-castle.war",
+    "vault_url" => "https://ssoqa1-vault.nexus.commercehub.com:8443",
+    "truststore_file" => "dev-truststore.jks",
+    "keystore_file" => "dev-keystore.jks"
+  },
+  "chub-plaza" => {
+    "app_url" => "file:///var/plaza/staged-plaza.war",
+    "cas_server_url" => "https://ssoqa1castle1.nexus.commercehub.com:8443",
+    "cas_service_url" => "http://ssoqa1plaza1.nexus.commercehub.com:8080/shiro-cas",
+    "graphite_host" => "stat02.nexus.commercehub.com"
+  }
 )
