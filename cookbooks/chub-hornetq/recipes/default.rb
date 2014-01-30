2592f50c36c36c135b2754688256772c5ba51015
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-01-30 11:36:48 -0500

    laying out the basics of hornetq

--- cookbooks/chub-hornetq/recipes/default.rb
+++ cookbooks/chub-hornetq/recipes/default.rb
@@ -16,3 +16,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+
+# Enforcing needed settings, but these should be applied at the Role level
+node.set['java']['install_flavor'] = 'oracle'
+node.set['java']['oracle']['accept_oracle_download_terms'] = true
+node.set['java']['jdk_version'] = 7
+
+node.set['tomcat']['base_version'] = 7
+node.set['tomcat']['loglevel'] = 'WARN'		# default is 'INFO'
+
+#if node[:instance_role] == 'vagrant'
+if Chef::Config[:solo]
+	node.set["tomcat"]["keystore_password"] = 'throwawaypassword'
+	node.set["tomcat"]["truststore_password"] = 'throwawaypassword'
+end
+
+include_recipe "java"
+include_recipe "tomcat"
+
+# Packaged Prereqs
+%w{
+	libaio1
+}.each do |pkg|
+	package pkg do
+		action :install
+	end
+end
+
+# # if local file cache is not available, download from internet
+# uri = URI(url)
+# request = Net::HTTP.new uri.host
+# response = request.request_head uri.path
+# if response.code.to_i == 200
+# 	# use local http
+# else
+# 	# use jboss.org site
+# 	# consider throwing a warning
+# end
+
