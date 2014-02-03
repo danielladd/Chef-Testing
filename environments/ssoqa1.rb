e60b22eab51969c55812766e4a3404ead89a51cf
David M. Carr <dcarr@commercehub.com>
2014-02-03 17:28:26 -0500

    ssoqa1: fix castle app_url

--- environments/ssoqa1.rb
+++ environments/ssoqa1.rb
@@ -19,7 +19,7 @@ default_attributes(
     "search_password" => "search"
   },
   "chub-castle" => {
-    "app_url" => "file:///var/castle/staged-castle.jar",
+    "app_url" => "file:///var/castle/staged-castle.war",
     "vault_url" => "https://ssoqa1-vault.nexus.commercehub.com:8443",
     "truststore_file" => "dev-truststore.jks",
     "keystore_file" => "dev-keystore.jks",
