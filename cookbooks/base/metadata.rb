0f064e6c57a45502751ae47450d514c7f98b265b
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-02-26 15:13:51 -0500

    adding chef-kick to the base metadata

--- cookbooks/base/metadata.rb
+++ cookbooks/base/metadata.rb
@@ -4,7 +4,8 @@ maintainer_email	'YOUR_EMAIL'
 license				'Apache 2.0'
 description			'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version				'0.1.5'
+version				'0.1.6'
 depends				"git"
 depends				"chef-client"
 depends				"ntp"
+depends				"chef-kick"
