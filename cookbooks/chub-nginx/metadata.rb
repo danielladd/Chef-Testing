a32217822e332e87cf6f622186afaeefd047db2a
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-02-27 15:45:25 -0500

    an initial basically-working custom nginx cookbook

---
+++ cookbooks/chub-nginx/metadata.rb
@@ -0,0 +1,9 @@
+name             'chub-nginx'
+maintainer       'CommerceHub Inc.'
+maintainer_email 'wgrunenwald@commercehub.com'
+license          'Apache 2.0'
+description      'Installs/Configures chub-nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'nginx'
