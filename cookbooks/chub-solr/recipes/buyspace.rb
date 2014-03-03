7b7a5a0487d142d6a8c2a23a5cbdd071ed7a9274
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-03-03 16:04:10 -0500

    most chub-solr bits are on the right box, but SOLR isn't running properly

--- cookbooks/chub-solr/recipes/buyspace.rb
+++ cookbooks/chub-solr/recipes/buyspace.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: chub-solr
-# Recipe:: buyspace
+# Recipe:: solr
 #
 # Copyright 2014, CommerceHub Inc.
 #
@@ -19,25 +19,104 @@
 
 include_recipe "chub-solr"
 
-#check the filenames & path on this
+#TODO: check the filenames & path on this
 execute 'Extract_SOLR_Tarball' do
-	command "tar -zxvf #{node['chub-solr']['base_dir']}/solr-#{node['chub-solr']['version']}.tar.gz -C #{node['chub-solr']['base_dir']}"
+	command "tar -zxvf #{node['chub-solr']['archive_dir']}/solr-#{node['chub-solr']['version']}.tgz -C #{node['chub-solr']['app_dir']}"
 	action :nothing
 end
 
-#check the filenames & paths
+#TODO: check the filenames & paths
 execute 'Extract_SOLR_Config_Zip' do
 	command "unzip #{node['chub-solr']['base_dir']}/solr.zip -d #{node['chub-solr']['config_dir']}"
 	# command "echo 'I'm totally extracting that thing you wanted now...'"
 	action :nothing
 end
 
+execute 'clear_tomcat_app_directory' do
+	command "rm -fr #{node['chub-solr']['app_dir']}/solr"
+	action :nothing
+end
+
+group "#{node['chub-solr']['group']}" do
+	action :create
+	system true
+end
+
+user "#{node['chub-solr']['user']}" do
+	comment "Application user for solr"
+	gid "#{node['chub-solr']['group']}"
+	system true
+end
+
 directory "#{node['chub-solr']['config_dir']}" do
-	action :create
+	owner "root"
+	group "#{node['chub-solr']['group']}"
+	mode 0755
 end
 
-directory "#{node['chub-solr']['base_dir']}" do
-	action :create
+directory "#{node['chub-solr']['app_dir']}" do
+	owner "#{node['chub-solr']['user']}"
+	group "#{node['chub-solr']['group']}"
+	mode 0774
 end
 
-# look at the chub-hornetq::buyspace.rb:~54 and chub-buyspace
+directory "#{node['chub-solr']['log_dir']}" do
+	owner "#{node['chub-solr']['user']}"
+	group "#{node['chub-solr']['group']}"
+	mode 0774
+end
+
+directory "#{node['chub-solr']['data_dir']}" do
+	owner "#{node['chub-solr']['user']}"
+	group "#{node['chub-solr']['group']}"
+	mode 0774
+end
+
+directory "#{node['chub-solr']['temp_dir']}" do
+	owner "tomcat#{node['tomcat']['base_version']}"
+	group "#{node['chub-solr']['group']}"
+	mode 0775
+end
+
+
+source_url = ''
+if node['instance_role'] == 'vagrant'
+	unless File.exists?("/vagrant/solr-#{node['chub-solr']['version']}.tgz")
+		`wget #{node['chub-hornetq']['origin_http_uri']} -P /vagrant`
+		#`wget http://archive.apache.org/dist/lucene/solr/#{node['chub-solr']['version']}/solr-#{node['chub-solr']['version']}.tgz -P /vagrant`
+	end
+	source_url = "file:///vagrant/solr-#{node['chub-solr']['version']}.tgz"
+else
+	source_url = node['chub-hornetq']['origin_http_uri']
+	log "Downloading file from the internet. Check on the internal URI." do
+		level :warn
+	end
+end
+
+#TODO: look at the chub-hornetq::solr.rb:~54 and chub-solr
+# App File
+remote_file "#{node['chub-solr']['archive_dir']}/solr-#{node['chub-solr']['version']}.tgz" do
+	# source "http://archive.apache.org/dist/lucene/solr/#{node['chub-solr']['version']}/solr-#{node['chub-solr']['version']}.tgz"
+	source source_url
+	owner "#{node['chub-solr']['user']}"
+	group "#{node['chub-solr']['group']}"
+	action :create_if_missing
+	notifies :run, 'execute[clear_tomcat_app_directory]', :immediately
+	notifies :run, 'execute[Extract_SOLR_Tarball]', :immediately
+	notifies :restart, "service[tomcat]", :delayed
+end
+
+# Config File
+remote_file "#{node['chub-solr']['base_dir']}/solr.zip" do
+	action :create_if_missing
+	source "http://mpbamboo.nexus.commercehub.com/artifact/BS-BSM/shared/build-latestSuccessful/solr.zip/solr.zip"
+	notifies :run, 'execute[Extract_SOLR_Config_Zip]', :immediately
+	mode 0755
+	# notifies :restart, "service[tomcat]", :delayed
+end
+
+
+
+
+
+#TODO: ? link /var/lib/tomcat7/webapps/SOLR to "#{node['chub-solr']['base_dir']}"-"#{node['chub-solr']['version']}""
