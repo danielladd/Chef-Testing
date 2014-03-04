b8c44835d76397ae9eec1a38def184422e6cc781
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-03-04 13:58:15 -0500

    creating empty chub-batchagent cookbook

---
+++ cookbooks/chub-batchagent/metadata.rb
@@ -0,0 +1,7 @@
+name             'chub-batchagent'
+maintainer       'CommerceHub Inc.'
+maintainer_email 'wgrunenwald@commercehub.com'
+license          'Apache 2.0'
+description      'Installs/Configures chub-batchagent'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
