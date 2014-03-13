c1fc5828c9e18eb2e3c04b2f1ad64718a708d8f0
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-03-13 10:29:11 -0400

    adding the chub-missioncontrol cookbook with some boilerplate

---
+++ cookbooks/chub-missioncontrol/attributes/default.rb
@@ -0,0 +1,8 @@
+default['java']['install_flavor']							= 'oracle'
+default['java']['oracle']['accept_oracle_download_terms']	= true
+default['java']['jdk_version']								= 7
+default['tomcat']['base_version']							= 7
+
+default['chub-missioncontrol']['config_dir']	= '/etc/missioncontrol'
+default['chub-missioncontrol']['app_dir']		= '/opt/missioncontrol'
+default['chub-missioncontrol']['log_dir']		= '/var/log/missioncontrol'
---
+++ cookbooks/chub-missioncontrol/metadata.rb
@@ -0,0 +1,11 @@
+name             'chub-missioncontrol'
+maintainer       'CommerceHub Inc.'
+maintainer_email 'wgrunenwald@commercehub.com'
+license          'Apache 2.0'
+description      'Installs/Configures chub-missioncontrol'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'java'
+depends 'tomcat'
+depends 'openssl'
