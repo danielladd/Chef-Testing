8b02ff994796c81c4c54cd83f3c79b5e8b4bdf70
Larry Zarou <lzarou@commercehub.com>
2014-03-20 10:42:35 -0400

    adding new recipe for hornetq

---
+++ cookbooks/chub-hornetq/recipes/mqdestinations.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: chub-hornetq
+# Recipe:: mqdestinations
+#
+# Copyright 2014, CommerceHub Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+prop_file = "#{node[:'chub-hornetq'][:staging_dir]}/#{node[:'chub-hornetq'][:destinations_deploy_prop]}"
+remote_file "#{node[:'chub-hornetq'][:staging_dir]}/#{node[:'chub-hornetq'][:destinations_jar_name]}" do
+  source node[:'chub-hornetq'][:destinations_deploy_jar_url]
+  notifies :create, "template[#{prop_file}]"	
+end
+
+template "#{prop_file}" do
+  source "destinations.deploy.properties.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  action :create
+  #TODO: This does not properly notify or re-execute deploy after property file is updated
+  notifies :run, "execute[deployDestinations]"
+end
+
+execute "deployDestinations" do
+  command "java -jar #{node[:'chub-hornetq'][:staging_dir]}/#{node[:'chub-hornetq'][:destinations_jar_name]} -d #{prop_file}"
+  cwd "#{node[:'chub-hornetq'][:staging_dir]}"
+  action :run
+  notifies :restart, "service[hornetq]", :delayed
+end
+
