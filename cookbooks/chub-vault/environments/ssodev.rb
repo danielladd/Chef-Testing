21a9e214929ffbf0b4edc77844b1650acb1de230
David M. Carr <dcarr@commercehub.com>
2014-03-27 10:17:55 -0400

    SSO-185 update vault cookbook to support configuring more stuff

--- cookbooks/chub-vault/environments/ssodev.rb
+++ cookbooks/chub-vault/environments/ssodev.rb
@@ -23,14 +23,20 @@ default_attributes(
     "host" => "stats02.nexus.commercehub.com"
   },
   "chub-vault" => {
-    "ldap_read_host" => "ssodev1-ldap-read.nexus.commercehub.com",
-    "ldap_write_host" => "ssodev1-ldap-write.nexus.commercehub.com",
+    "ldap" => {
+      "read" => {
+        "host" => "ssodev1-ldap-read.nexus.commercehub.com",
+        "password" => "search"
+      },
+      "write" => {
+        "host" => "ssodev1-ldap-write.nexus.commercehub.com",
+        "password" => "rootpw"
+      }
+    },
     "database" => {
       "url" => "jdbc:sqlserver://sqlvm81.nexus.commercehub.com;databaseName=ssodev-vault",
       "user" => "devusr01",
       "password" => "usrdev"
-    },
-    "search_password" => "search",
-    "write_password" => "rootpw"
+    }
   }
 )
