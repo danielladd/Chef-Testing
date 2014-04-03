cdb35d5aa75e0835e94770f854b8c9cfa03a751f
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-04-03 17:35:43 -0400

    update JAVA_HOME

--- environments/ptboxes.rb
+++ environments/ptboxes.rb
@@ -55,7 +55,7 @@ default_attributes(
   "java" => {
     "windows" => {
       "url" => "http://artifactory01/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-windows-x64.exe",
-      "home" => "C:\Program Files\Java"
+      "home" => 'C:\Program Files\Java\jdk1.7.0_51'
     }
   }
 )
