074f58d86171ad5d632a9353d5d2c933b0032b90
David M. Carr <dcarr@commercehub.com>
2014-04-04 13:06:40 -0400

    update chub-vault cookbook

--- environments/ssodev1.rb
+++ environments/ssodev1.rb
@@ -18,7 +18,7 @@ cookbook_versions({
   "chub-openldap" => "= 1.12.15",
   "chub-plaza" => "= 0.1.4",
   "chub-sensu" => "= 0.1.7",
-  "chub-vault" => "= 0.2.2",
+  "chub-vault" => "= 0.3.0",
   "chub_sensu_sso" => "= 0.1.11",
   "cron" => "= 1.2.6",
   "dmg" => "= 2.1.4",
