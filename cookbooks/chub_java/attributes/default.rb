5b22dc3e3e1c123ea28359b2c53514e5c0914bcf
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-04-14 15:59:43 -0400

    adding chub_java cookbook to the repo

---
+++ cookbooks/chub_java/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:java][:oracle][:accept_oracle_download_terms] = true
+default[:java][:jdk]['7'][:x86_64][:url] = 'http://artifactory01.nexus.commercehub.com/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz'
+default[:java][:jdk]['8'][:x86_64][:url] = 'http://artifactory01.nexus.commercehub.com/artifactory/java-distributions/jdk/8-b132/jdk-8-linux-x64.tar.gz'
