a86fb5824d33909ffaedad41dd2be55feddd0125
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-04-15 14:34:31 -0400

    FQDN the artifactory hosts

--- environments/albersdev.rb
+++ environments/albersdev.rb
@@ -55,7 +55,7 @@ cookbook_versions({
 default_attributes(
   "java" => {
     "windows" => {
-      "url" => "http://artifactory01/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-windows-x64.exe",
+      "url" => "http://artifactory01.nexus.commercehub.com/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-windows-x64.exe",
       "home" => 'C:\Program Files\Java\jdk1.7.0_51'
     }
   }
