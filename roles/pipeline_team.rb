e4d8fc9c44c57e5da6934648548797c74f23644b
Larry Zarou <lzarou@commercehub.com>
2014-04-16 12:20:23 -0400

    updating roles for sensu_mailing and pipeline_team

--- roles/pipeline_team.rb
+++ roles/pipeline_team.rb
@@ -4,11 +4,5 @@ default_attributes "sensu" => {
     "rabbitmq" => {
         "host" => "sensu01.nexus.commercehub.com"
     }
-},"chub_sensu" => {
-    "email" => {
-        "subscriptions_mail_to" => {
-            "pipeline_team" => "pipeline@commercehub.com",
-        }
-    }
 }
 run_list "recipe[chub_pipeline::sensu_client]"
