37aa080c43682d5979c3afd5dacc333303c896bb
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-04-16 08:08:41 -0400

    removing java attributes from base role

--- roles/base.rb
+++ roles/base.rb
@@ -1,10 +1,6 @@
 name "base"
 description "A basic role applicable to all nodes"
-default_attributes "java" => {
-	'install_flavor' => 'oracle',
-	'oracle' => { 'accept_oracle_download_terms' => true },
-	'jdk_version' => 7
-}, "ntp" => {
+default_attributes "ntp" => {
 	'servers' => [ 
 		'time.commercehub.com',
 		'0.pool.ntp.org',
