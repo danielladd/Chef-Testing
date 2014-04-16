c3dd39728aa58d27e9b07eeec492b6b29e233e44
Larry Zarou <lzarou@commercehub.com>
2014-04-16 12:48:16 -0400

    updates to readme and sensu_server recipe to fix vmstat check

--- cookbooks/chub_pipeline/CHANGES.md
+++ cookbooks/chub_pipeline/CHANGES.md
@@ -1,3 +1,7 @@
+# 0.1.2
+
+* Updates to readme and quick bug fix for graphite handler and vmstat check
+
 # 0.1.1
 
 * Migrated in graphite handler and check from chub_sensu, also removed ALL from client setup
--- cookbooks/chub_pipeline/metadata.rb
+++ cookbooks/chub_pipeline/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lzarou@commercehub.com'
 license          'All rights reserved'
 description      'Installs/Configures Sensu in the CommerceHub way for Pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends "sensu"
 depends "base"
