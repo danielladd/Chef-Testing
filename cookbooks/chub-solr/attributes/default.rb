3f3d891dd680badc7cbf8496511af1c80737381a
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-04-16 13:36:04 -0400

    updating chub-solr cookbook to use chub_java cookbook

--- cookbooks/chub-solr/attributes/default.rb
+++ cookbooks/chub-solr/attributes/default.rb
@@ -12,3 +12,5 @@ default['chub-solr']['log_dir']		= '/var/log/solr'
 default['chub-solr']['origin_http_uri']	= "http://archive.apache.org/dist/lucene/solr/#{node['chub-solr']['version']}/solr-#{node['chub-solr']['version']}.tgz"
 
 default['chub-solr']['cores']		= [	'marketplaceProduct', 'product', 'user', 'company', 'help', 'channelProduct' ]
+default['chub-solr']['port']		= '8983'
+#normal['tomcat']['base_version']	= '7'
--- cookbooks/chub-solr/metadata.rb
+++ cookbooks/chub-solr/metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures chub-solr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "java", "= 1.16.2"
+depends "chub_java"
 depends "tomcat"
 depends "openssl"
