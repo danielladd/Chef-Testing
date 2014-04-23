ad36fc2004f82f8e73ad05db957397fafe9477a9
Larry Zarou <lzarou@commercehub.com>
2014-04-23 09:28:38 -0400

    fixing nested json

--- roles/pipeline_team.rb
+++ roles/pipeline_team.rb
@@ -4,5 +4,8 @@ default_attributes "sensu" => {
     "rabbitmq" => {
         "host" => "sensu01.nexus.commercehub.com"
     }
+},
+"ssh_keys" => {
+        "lzarou" => "lzarou"
 }
 run_list "recipe[chub_pipeline::sensu_client]"
