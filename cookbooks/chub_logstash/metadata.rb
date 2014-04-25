af4e7ce0cb70646dfaa25bd2aa02827742306f99
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-04-25 18:35:13 -0400

    logstash server role and cookbook

---
+++ cookbooks/chub_logstash/metadata.rb
@@ -0,0 +1,9 @@
+name             'chub_logstash'
+maintainer       'Commerce Technology Inc'
+maintainer_email 'dcross@commercehub.com'
+license          'All rights reserved'
+description      'Installs/Configures chub_logstash'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.12'
+
+depends "elasticsearch"
