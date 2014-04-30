7b075247a8ccdc68aa67e7e5994c595e12e9681a
Erik Stegmann <estegmann@commercehub.com>
2014-04-30 09:42:35 -0400

    Revert context path change for CSD

--- cookbooks/chub-csdashboard/attributes/default.rb
+++ cookbooks/chub-csdashboard/attributes/default.rb
@@ -3,6 +3,6 @@ default['chub-csdashboard']['app']['config_dir']	= '/etc/csdashboard'
 default['chub-csdashboard']['app']['deploy_dir'] = '/opt/csdashboard'
 default['chub-csdashboard']['app']['log_dir'] = '/var/log/csdashboard'
 default['chub-csdashboard']['app']['config_file_name'] = 'csdashboard-config.groovy'
-default['chub-csdashboard']['app']['app_name'] = 'csd'
+default['chub-csdashboard']['app']['app_name'] = 'csdashboard'
 default['chub-csdashboard']['app']['jar_file_url'] = 'OVERRIDE ME'
 default['chub-csdashboard']['app']['touchfile'] = "node['chub-csdashboard']['app']['app_dir']/deployed"
