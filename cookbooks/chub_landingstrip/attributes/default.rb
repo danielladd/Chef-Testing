81a0a78d9035a6f2de3c1b4211e1a6e9e124603d
Erik Stegmann <estegmann@commercehub.com>
2014-05-06 08:19:05 -0400

    Modify landingstrip recipe to perform a safer deploy of landingstrip

--- cookbooks/chub_landingstrip/attributes/default.rb
+++ cookbooks/chub_landingstrip/attributes/default.rb
@@ -1,6 +1,7 @@
-default['chub_landingstrip']['app']['port'] = "9100"
-default['chub_landingstrip']['app']['deploy_dir'] = '/opt/landingstrip'
-default['chub_landingstrip']['app']['config_file_name'] = 'landingstrip-config.groovy'
-default['chub_landingstrip']['app']['app_name'] = 'landingstrip'
-default['chub_landingstrip']['app']['jar_file_url'] = 'OVERRIDE ME'
-default['chub_landingstrip']['app']['touchfile'] = "node['chub_landingstrip']['app']['app_dir']/deployed"
+default[:chub_landingstrip][:app][:port] = "9100"
+default[:chub_landingstrip][:app][:deploy_dir] = '/opt/landingstrip'
+default[:chub_landingstrip][:app][:staging_dir] = '/opt/landingstrip-staging'
+default[:chub_landingstrip][:app][:config_file_name] = 'landingstrip-config.groovy'
+default[:chub_landingstrip][:app][:app_name] = 'landingstrip'
+default[:chub_landingstrip][:app][:jar_file_url] = 'OVERRIDE ME'
+default[:chub_landingstrip][:app][:touchfile] = "#{node[:chub_landingstrip][:deploy_dir]}/deployed"
--- cookbooks/chub_landingstrip/recipes/default.rb
+++ cookbooks/chub_landingstrip/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: chub_landingstrip
+# Cookbook Name: chub_landingstrip
 # Recipe:: default
 #
 # Copyright 2014, CommerceHub Inc.
@@ -40,23 +40,47 @@ group "chub_landingstrip" do
     members ["chub_landingstrip", "chadmin"]
 end
 
-directory node['chub_landingstrip']['app']['deploy_dir'] do
+unless File.exists?("#{node[:chub_landingstrip][:app][:touchfile]}")
+
+directory node[:chub_landingstrip][:app][:staging_dir] do
   action :create
   owner "chub_landingstrip"
   group "chub_landingstrip"
   mode 0777
 end
 
-touchfile = node['chub_landingstrip']['app']['touchfile']
+directory node[:chub_landingstrip][:app][:deploy_dir] do
+  action :create
+  owner "chub_landingstrip"
+  group "chub_landingstrip"
+  mode 0777
+end
 
-remote_file "#{node['chub_landingstrip']['app']['deploy_dir']}/#{node['chub_landingstrip']['app']['app_name']}.jar" do
-  source "#{node['chub_landingstrip']['app']['jar_file_url']}"
-  not_if do
-    File.exists?(touchfile)
-  end
+service "landingstrip" do
+    provider Chef::Provider::Service::Upstart
+    action [ "disable", "stop" ]
+end
+
+remote_file "#{node[:chub_landingstrip][:app][:staging_dir]}/#{node[:chub_landingstrip][:app][:app_name]}.jar" do
+  source "#{node[:chub_landingstrip][:app][:jar_file_url]}"
   owner "chub_landingstrip"
   group "chub_landingstrip"
   action :create	# This should pull the file down forcefully
+end
+
+file "#{node[:chub_landingstrip][:app][:deploy_dir]}/#{node[:chub_landingstrip][:app][:app_name]}.jar" do
+    action   :delete
+    mode     "0755"
+    owner    "chub_landingstrip"
+    group    "chub_landingstrip"
+end
+
+remote_file "Copy deploy jar file from staging" do 
+  path "#{node[:chub_landingstrip][:app][:deploy_dir]}/#{node[:chub_landingstrip][:app][:app_name]}.jar"
+  source "file://#{node[:chub_landingstrip][:app][:staging_dir]}/#{node[:chub_landingstrip][:app][:app_name]}.jar"
+  owner 'root'
+  group 'root'
+  mode 0755
 end
 
 template "/etc/init/landingstrip.conf" do
@@ -64,18 +88,26 @@ template "/etc/init/landingstrip.conf" do
     owner "root"
     group "root"
     mode 0644
-    notifies "restart", "service[landingstrip]", :delayed
 end
 
 link "/etc/landingstrip" do
-  to "#{node['chub_landingstrip']['app']['deploy_dir']}/config"
+  to "#{node[:chub_landingstrip][:app][:deploy_dir]}/config"
 end
 
 link "/var/log/landingstrip" do
-  to "#{node['chub_landingstrip']['app']['deploy_dir']}/log"
+  to "#{node[:chub_landingstrip][:app][:deploy_dir]}/log"
 end
 
 service "landingstrip" do
     provider Chef::Provider::Service::Upstart
     action [ "enable", "start" ]
 end
+
+file node[:chub_landingstrip][:app][:touchfile] do
+    action   :touch
+    mode     "0755"
+    owner    "chub_landingstrip"
+    group    "chub_landingstrip"
+end
+
+end#close unless block
