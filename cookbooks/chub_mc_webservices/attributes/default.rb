3f60e1be69bfa6dbdef0fc8276047d5d9801fff7
Erik Stegmann <estegmann@commercehub.com>
2014-05-07 12:38:16 -0400

    MC Salesforce webservice recipe changes

--- cookbooks/chub_mc_webservices/attributes/default.rb
+++ cookbooks/chub_mc_webservices/attributes/default.rb
@@ -1,11 +1,21 @@
-default[:chub_mc_webservice][:flight][:deploy_dir] = opt/mc-webservices/flight
-default[:chub_mc_webservice][:flight][:staging_dir] = opt/mc-webservices-staging/flight
-default[:chub_mc_webservice][:flight][:jar_file_url] = OVERRIDE ME
+default[:chub_mc_webservice][:flight][:deploy_dir] = 'opt/mc-webservices/flight'
+default[:chub_mc_webservice][:flight][:staging_dir] = 'opt/mc-webservices-staging/flight'
+default[:chub_mc_webservice][:flight][:jar_file_url] = 'OVERRIDE ME'
 
-default[:chub_mc_webservice][:salesforce][:deploy_dir] = opt/mc-webservices/salesforce
-default[:chub_mc_webservice][:salesforce][:staging_dir] = opt/mc-webservices-staging/salesforce
-default[:chub_mc_webservice][:salesforce][:jar_file_url] = OVERRIDE ME
+default[:chub_mc_webservice][:salesforce][:deploy_dir] = 'opt/mc-webservices/salesforce'
+default[:chub_mc_webservice][:salesforce][:staging_dir] = 'opt/mc-webservices-staging/salesforce'
+default[:chub_mc_webservice][:salesforce][:jar_file_url] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:jar_file_name] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:config_file_name] = 'salesforce_webservice_config.yml'
+default[:chub_mc_webservice][:salesforce][:touchfile] = "#{node[:chub_mc_webservice][:salesforce][:deploy_dir]}/deployed"
+default[:chub_mc_webservice][:salesforce][:salesforceClient_client] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:salesforceClient_host] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:salesforceClient_clientId] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:salesforceClient_clientSecret] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:salesforceClient_username] = 'OVERRIDE ME'
+default[:chub_mc_webservice][:salesforce][:salesforceClient_password] = 'OVERRIDE ME'
 
-default[:chub_mc_webservice][:netsuite][:deploy_dir] = opt/mc-webservices/netsuite
-default[:chub_mc_webservice][:netsuite][:staging_dir] = opt/mc-webservices-staging/netsuite
-default[:chub_mc_webservice][:netsuite][:jar_file_url] = OVERRIDE ME
+
+default[:chub_mc_webservice][:netsuite][:deploy_dir] = 'opt/mc-webservices/netsuite'
+default[:chub_mc_webservice][:netsuite][:staging_dir] = 'opt/mc-webservices-staging/netsuite'
+default[:chub_mc_webservice][:netsuite][:jar_file_url] = 'OVERRIDE ME'
--- cookbooks/chub_mc_webservices/recipes/salesforce_webservice.rb
+++ cookbooks/chub_mc_webservices/recipes/salesforce_webservice.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: chub_mc_webservice
-# Recipe:: flight_webservice
+# Recipe:: salesforce_webservice
 #
 # Copyright 2014, CommerceHub Inc.
 #
@@ -17,3 +17,101 @@
 # limitations under the License.
 #
 
+group "chub_salesforce_webservice" do
+    action :create
+    system true
+end
+
+user "chub_salesforce_webservice" do
+    comment "Application user for salesforce_webservice"
+    gid "chub_salesforce_webservice"
+    system true
+end
+
+user "chadmin" do
+    # Placeholder user; in "real" VMs, this user is expected to already exist.
+    # This definition makes it so that in Vagrant VMs, the user exists so that the group definition below doesn't fail.
+end
+
+group "chub_salesforce_webservice" do
+    action :modify
+    append true
+    members ["chub_salesforce_webservice", "chadmin"]
+end
+
+unless File.exists?("#{node[:chub_mc_webservice][:salesforce][:touchfile]}")
+
+directory node[:chub_mc_webservice][:salesforce][:staging_dir] do
+  action :create
+  owner "chub_salesforce_webservice"
+  group "chub_salesforce_webservice"
+  mode 0777
+end
+
+directory node[:chub_mc_webservice][:salesforce][:staging_dir] do
+  action :create
+  owner "chub_salesforce_webservice"
+  group "chub_salesforce_webservice"
+  mode 0777
+end
+
+service "chub_salesforce_webservice" do
+    provider Chef::Provider::Service::Upstart
+    action [ "disable", "stop" ]
+end
+
+remote_file "#{node[:chub_mc_webservice][:salesforce][:staging_dir]}/#{node[:chub_mc_webservice][:salesforce][:jar_file_name]}" do
+  source "#{node[:chub_mc_webservice][:salesforce][:jar_file_url]}"
+  owner "chub_salesforce_webservice"
+  group "chub_salesforce_webservice"
+  action :create	# This should pull the file down forcefully
+end
+
+file "#{node[:chub_mc_webservice][:salesforce][:deploy_dir]}/#{node[:chub_mc_webservice][:salesforce][:jar_file_name]}" do
+    action   :delete
+    mode     "0755"
+    owner    "chub_salesforce_webservice"
+    group    "chub_salesforce_webservice"
+end
+
+remote_file "Copy deploy jar file from staging" do 
+  path "#{node[:chub_mc_webservice][:salesforce][:deploy_dir]}/#{node[:chub_mc_webservice][:salesforce][:jar_file_name]}"
+  source "file://#{node[:chub_mc_webservice][:salesforce][:staging_dir]}/#{node[:chub_mc_webservice][:salesforce][:jar_file_name]}"
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+#create yaml config file
+template "#{node[:chub_mc_webservice][:salesforce][:deploy_dir]}/salesforce_webservice_config.yml" do
+    source "salesforce_webservice_config.yml.erb"
+    owner "root"
+    group "root"
+    mode 0644
+	action :create
+end
+
+#create salesforce webservice service wrapper
+template "/etc/init/salesforce_webservice.conf" do
+    source "salesforce_webservice.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+	action :create
+end
+
+service "chub_salesforce_webservice" do
+    provider Chef::Provider::Service::Upstart
+    action [ "enable", "start" ]
+end
+
+file node[:chub_mc_webservice][:salesforce][:touchfile] do
+    action   :touch
+    mode     "0755"
+    owner    "chub_salesforce_webservice"
+    group    "chub_salesforce_webservice"
+end
+
+end#close unless block
+
+end
