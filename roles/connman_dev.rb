112c80a4cde22ef01397823285a62022d1805cbe
Erik Stegmann <estegmann@commercehub.com>
2014-05-12 08:26:05 -0400

    MC role file fixes

--- roles/connman_dev.rb
+++ roles/connman_dev.rb
@@ -1,6 +1,6 @@
 name "connman_dev"
 description "A linux machine running connman (in development)"
-run_list "recipe[base]", "recipe[ulimit]", "recipe[mongodb::10gen_repo]", "recipe[mongodb]", "recipe[chub_mc_app]",
+run_list "recipe[base]", "recipe[ulimit]", "recipe[mongodb::10gen_repo]", "recipe[mongodb]", "recipe[chub_mc_app]"
 default_attributes "chub_mc_app" => {
 	"port" => "9400",
 	"app_name" => "connman",
