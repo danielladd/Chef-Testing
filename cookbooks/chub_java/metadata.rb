3647f1d54ad536e87be4b7ed35a96304aee6fd25
David M. Carr <dcarr@commercehub.com>
2014-05-13 10:08:18 -0400

    chub_java 0.1.1

--- cookbooks/chub_java/metadata.rb
+++ cookbooks/chub_java/metadata.rb
@@ -4,9 +4,10 @@ maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures chub_java'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'java', '= 1.22.0'
+depends 'windows', '~> 1.30.0'
 
 supports 'ubuntu'
-# TODO: add support for windows and test
+supports 'windows'
