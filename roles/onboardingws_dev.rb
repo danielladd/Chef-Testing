9c0038a113003b5bbb853fce0378a1e7db854fcb
chris cucinella <ccucinella@commercehub.com>
2014-05-16 11:07:07 -0400

    update recipes and roles for MC web services

--- roles/onboardingws_dev.rb
+++ roles/onboardingws_dev.rb
@@ -4,6 +4,6 @@ run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservi
 default_attributes "chub_mc_webservice" => {
 	"onboarding" => {
     	"jar_file_name" => "OnboardingService-standalone.jar",
-		"jar_file_url" => "bamboom1:8085/browse/MC-WSON/latest/artifact/shared/Onboarding-Web-Service-Jar/OnboardingService-standalone.jar"
+		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSON/latest/artifact/shared/Onboarding-Web-Service-Jar/OnboardingService-standalone.jar?os_username=mess&os_password=messuser"
 		},
 }
