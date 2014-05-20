18981b2fe06287440e4c4a0e2585cf2cd5c34b29
Alex Heiss <aheiss@commercehub.com>
2014-05-20 11:57:54 -0400

    Bump Klerk cookbook version, to support API web node environment change. (see mpqa1 environment for associated change)
    Update the buyspace api, api job, wapp roles with attributes to reflect these roles.

--- roles/buyspace_api_job_runner.rb
+++ roles/buyspace_api_job_runner.rb
@@ -1,6 +1,12 @@
 name "buyspace_api_job_runner"
 description "role defining buyspace api and runs jobs"
-default_attributes "chub-buyspace" => {
-    "api" => "true",
-    "jobRunner" => "true"
+override_attributes "chub-buyspace" => {
+    "taskScheduler" => {
+      "enabled" => true
+    }, 
+    "grails" => {
+      "serverURL" => "http://mpqa1-api.commercehub.com",
+      "apiServerURL" => "http://mpqa1-api.commercehub.com"
+    }   
 }
+run_list "role[base]", "recipe[chub-buyspace::upgrade_buyspace]"
