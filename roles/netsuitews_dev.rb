20c6b4baea28446dac4ed234e6f87320f66f9076
Erik Stegmann <estegmann@commercehub.com>
2014-05-27 13:24:33 -0400

    Create roles for test and prod versions of mission control webservices

--- roles/netsuitews_dev.rb
+++ roles/netsuitews_dev.rb
@@ -1,8 +1,9 @@
 name "netsuitews_dev"
 description "A linux machine running netsuite webservice(in development)"
-run_list "recipe[base]", "recipe[chub_mc_webservices]"
-default_attributes "chub_mc_app" => {
-	"port" => "9400",
-	"app_name" => "catalog",
-	"jar_file_url" => "http://bamboom1:8085/browse/MC-CNM/latest/artifact/shared/Catalog/catalog.jar?os_username=mess&os_password=messuser"
+run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservices::netsuite_webservice]"
+default_attributes  "chub_mc_webservice" => {
+	"netsuite" => {
+    	"jar_file_name" => "billing-standalone.jar",
+		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSNET/latest/artifact/shared/NetSuite-Web-Service-Jar/billing-standalone.jar?os_username=mess&os_password=messuser"
+		}
 }
