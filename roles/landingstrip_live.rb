1412049c78e6c54c171d906faa745974454d36b7
Chris Cucinella <ccucinella@commercehub.com>
2014-05-28 11:33:36 -0400

    updates - added live recipes for mission control

---
+++ roles/landingstrip_live.rb
@@ -0,0 +1,26 @@
+name "landingstrip_live"
+description "A linux machine running landingstrip (in development)"
+run_list "recipe[base]", "recipe[ulimit]", "recipe[mongodb::10gen_repo]", "recipe[mongodb]", "recipe[chub_mc_app]"
+default_attributes "chub_mc_app" => {
+	"port" => "9100",
+	"app_name" => "landingstrip",
+	"jar_file_url" => "http://artifactory01/artifactory/simple/inteng-snapshot/landingstrip/0.1/landingstriplive.jar"
+},
+"ulimit" => {
+  "users" => {
+	"mongodb" => {
+	  "filehandle_limit" => 64000,
+	  "process_limit" => 32000,
+	  "memory_limit" => "unlimited"
+	},
+	"root" => {
+	  "filehandle_limit" => 64000,
+	  "process_limit" => 32000,
+	  "memory_limit" => "unlimited"
+	}
+  }
+},
+"mongodb" => {
+  "enable_rest" => true,
+  "dbpath" => "/data/db"
+}
