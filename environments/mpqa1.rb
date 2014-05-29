89a3055e064142bbc132e7618402c0655d9640cf
Alex Heiss <aheiss@commercehub.com>
2014-05-29 10:45:01 -0400

    Sync both qa envs.

--- environments/mpqa1.rb
+++ environments/mpqa1.rb
@@ -2,7 +2,7 @@ name "mpqa1"
 description "marketplace qa environment 1"
 cookbook "base", "= 0.1.16"
 cookbook "chub-klerk", "= 1.0.1"
-cookbook "chub-buyspace", "= 0.2.2"
+cookbook "chub-buyspace", "= 0.2.3"
 default_attributes "chub-klerk" => {
     "mongo_addresses" => [ "mpqa03:27777", "mpqa05:27777" ],
     "quartz_database_url" => "jdbc:jtds:sqlserver://sqlvm81/mpqa1_klerk_quartz;user=devusr01;password=usrdev",
