f579daa80bbe23bcb98200b75c0b1852cadf86c8
Lindsay Kulzer <lkulzer@commercehub.com>
2014-06-06 10:40:19 -0400

    fix attributes in new mission control connmanws roles

--- roles/connmanws_dev.rb
+++ roles/connmanws_dev.rb
@@ -2,7 +2,7 @@ name "connmanws_dev"
 description "A linux machine running connection manager WS (in development)"
 run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservices::connman_webservice]"
 default_attributes "chub_mc_webservice" => {
-	"onboarding" => {
+	"connman" => {
     	"jar_file_name" => "OnboardingService-standalone.jar",
 		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSON/latest/artifact/shared/Onboarding-Web-Service-Jar/OnboardingService-standalone.jar?os_username=mess&os_password=messuser"
 		}
--- roles/connmanws_live.rb
+++ roles/connmanws_live.rb
@@ -1,8 +1,8 @@
-name "onboardingws_live"
-description "A linux machine running onboarding WS (in production)"
-run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservices::onboarding_webservice]"
+name "connmanws_live"
+description "A linux machine running connman WS (in production)"
+run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservices::connman_webservice]"
 default_attributes "chub_mc_webservice" => {
-	"onboarding" => {
+	"connman" => {
     	"jar_file_name" => "OnboardingService-standalone.jar",
 		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSON/latest/artifact/shared/Onboarding-Web-Service-Jar-Live/OnboardingService-standalone.jar?os_username=mess&os_password=messuser"
 		}
