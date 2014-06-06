d20677da18ff8415dc45631fa0247ef38e1f7124
Lindsay Kulzer <lkulzer@commercehub.com>
2014-06-06 16:01:21 -0400

    fix mc connman ws artifact location in role

--- roles/connmanws_dev.rb
+++ roles/connmanws_dev.rb
@@ -3,7 +3,8 @@ description "A linux machine running connection manager WS (in development)"
 run_list "recipe[base]", "recipe[chub_mc_webservices]", "recipe[chub_mc_webservices::connman_webservice]"
 default_attributes "chub_mc_webservice" => {
 	"connman" => {
-    	"jar_file_name" => "OnboardingService-standalone.jar",
-		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSON/latest/artifact/shared/Onboarding-Web-Service-Jar/OnboardingService-standalone.jar?os_username=mess&os_password=messuser"
+    	"jar_file_name" => "ConnMgrService-standalone.jar",
+		"jar_file_url" => "http://bamboom1:8085/browse/MC-WSCON/latest/artifact/shared/ConnMan-Web-Service-Jar/ConnMgrService-standalone.jar?os_username=mess&os_password=messuser"
 		}
 }
+
