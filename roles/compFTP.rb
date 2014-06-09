e554774657baf8e76adc5a7d7a34377da3787674
Andrew Cotton <acotton@commercehub.com>
2014-06-09 11:20:21 -0400

    Roles for new SFTP services

---
+++ roles/compFTP.rb
@@ -0,0 +1,10 @@
+name "compFTP"
+description "A windows machine running CompleteFTP"
+default_attributes(
+  "java" => {
+    "windows" => {
+      "url" => "http://artifactory01.nexus.commercehub.com/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-windows-x64.exe",
+      "home" => 'C:\Program Files\Java\jdk1.7.0_51'
+    }
+  }
+run_list "recipe[chub_windows]"
