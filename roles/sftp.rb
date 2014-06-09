affe79e0718d05667ba7ba04937a4f530fa1614d
Andrew Cotton <acotton@commercehub.com>
2014-06-09 11:22:54 -0400

    Roles for new SFTP services fixed

--- roles/sftp.rb
+++ roles/sftp.rb
@@ -6,5 +6,5 @@ default_attributes(
       "url" => "http://artifactory01.nexus.commercehub.com/artifactory/java-distributions/jdk/7u51-b13/jdk-7u51-windows-x64.exe",
       "home" => 'C:\Program Files\Java\jdk1.7.0_51'
     }
-  }
+  })
 run_list "recipe[chub_windows]"
