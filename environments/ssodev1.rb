3259c329f8bbe65a6138e8afb1d67dbf5e8abea9
David M. Carr <dcarr@commercehub.com>
2014-06-10 11:43:31 -0400

    update base cookbook used by sso environments

--- environments/ssodev1.rb
+++ environments/ssodev1.rb
@@ -3,7 +3,7 @@ description "SSO development environment 1"
 cookbook_versions({
   "apache2" => "= 1.7.0",
   "apt" => "= 2.3.8",
-  "base" => "= 0.1.17",
+  "base" => "= 0.1.19",
   "bluepill" => "= 2.3.1",
   "build-essential" => "= 1.4.2",
   "chef-client" => "= 3.0.6",
