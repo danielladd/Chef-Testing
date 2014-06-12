549353468b62743855a567f7773675a0d2e73f8d
Andrew Cotton <acotton@commercehub.com>
2014-06-12 11:46:04 -0400

    Fixing issue that Cross will address

--- environments/albers_dev.rb
+++ environments/albers_dev.rb
@@ -2,9 +2,7 @@ name "albers_dev"
 description "Albers Development Environment"
 override_attributes(
   "node" => {
-	"java_url" => ["http://bamboom1:8085/browse/AL-ASNAP/latest/artifact/shared/Albers-Service-Shadow-Jar/AlbersService-shadow.jar?os_username=chef_user&os_password=chef_user"]
-    }
-)
+	"java_url" => ["http://bamboom1:8085/browse/AL-ASNAP/latest/artifact/shared/Albers-Service-Shadow-Jar/AlbersService-shadow.jar?os_username=chef_user&os_password=chef_user"]})
 
 default_attributes(
   "elasticsearch" => {
