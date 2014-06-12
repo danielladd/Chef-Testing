e811fbda451d6d570167dc862f545f7a30f167be
Larry Zarou <lzarou@commercehub.com>
2014-06-12 11:21:53 -0400

    fixing role

--- roles/berkshelf_api.rb
+++ roles/berkshelf_api.rb
@@ -14,7 +14,7 @@ default_attributes "berkshelf_api" => {
         }
       ]
     },
-    "host" => node[:fqdn],
-    "home_path" => node[:berkshelf_api][:home]
+    "host" => "#{node[:fqdn]}",
+    "home_path" => "#{node[:berkshelf_api][:home]}"
   }
 run_list "recipe[chub_berkshelf_api::default]"
