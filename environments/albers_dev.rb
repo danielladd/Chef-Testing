efc28208d5ee529403820a11315f81cfbe729387
Andrew Cotton <acotton@commercehub.com>
2014-06-12 10:40:24 -0400

    adding dependencies

--- environments/albers_dev.rb
+++ environments/albers_dev.rb
@@ -5,3 +5,16 @@ override_attributes(
 	"java_url" => ["http://bamboom1:8085/browse/AL-ASNAP/latest/artifact/shared/Albers-Service-Shadow-Jar/AlbersService-shadow.jar?os_username=chef_user&os_password=chef_user"]
     }
 )
+
+default_attributes(
+  "elasticsearch" => {
+    "version" => "1.1.0",
+    "cluster" => {
+      "name" => "esdev1"
+    }
+  }, 
+  "chub_logstash" => {
+    "endpoint" => "lsdev.nexus.commercehub.com",
+    "esgateway" => "esdev.nexus.commercehub.com"
+  }
+)
