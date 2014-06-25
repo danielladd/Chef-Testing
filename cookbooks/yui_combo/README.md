e97a95b9e4ca6c3f663f035a1bae77fa2d68af89
Justin DiPierro <jdipierro@commercehub.com>
2014-06-25 12:43:18 -0400

    yui_combo: Cookbook for spinning up a YUI Combo server

---
+++ cookbooks/yui_combo/README.md
@@ -0,0 +1,51 @@
+# yui_combo-cookbook
+
+TODO: Enter the cookbook description here.
+
+## Supported Platforms
+
+TODO: List your supported platforms.
+
+## Attributes
+
+<table>
+  <tr>
+    <th>Key</th>
+    <th>Type</th>
+    <th>Description</th>
+    <th>Default</th>
+  </tr>
+  <tr>
+    <td><tt>['yui_combo']['bacon']</tt></td>
+    <td>Boolean</td>
+    <td>whether to include bacon</td>
+    <td><tt>true</tt></td>
+  </tr>
+</table>
+
+## Usage
+
+### yui_combo::default
+
+Include `yui_combo` in your node's `run_list`:
+
+```json
+{
+  "run_list": [
+    "recipe[yui_combo::default]"
+  ]
+}
+```
+
+## Contributing
+
+1. Fork the repository on Github
+2. Create a named feature branch (i.e. `add-yui_combo-recipe`)
+3. Write you change
+4. Write tests for your change (if applicable)
+5. Run the tests, ensuring they all pass
+6. Submit a Pull Request
+
+## License and Authors
+
+Author:: CommerceHub Inc. (<jdipierro@commercehub.com>)
---
+++ cookbooks/yui_combo/recipes/default.rb
@@ -0,0 +1,84 @@
+#
+# Cookbook Name:: yui_combo
+# Recipe:: default
+#
+# Copyright 2014, CommerceHub
+#
+# All rights reserved - Do Not Redistribute
+#
+# Author: Justin DiPierro
+
+include_recipe "apt"
+include_recipe "node"
+
+service_name 	= "combo"
+group_name    = service_name
+user_name     = service_name
+
+group group_name do
+  action :create
+  system true
+  append true
+end
+
+user user_name do
+  comment "Application user for the YUI Combo Server"
+  gid group_name
+  system true
+end
+
+user "chadmin" do
+  # Placeholder user; in "real" VMs, this user is expected to already exist.
+  # This definition makes it so that in Vagrant VMs, the user exists so that the group definition below doesn't fail.
+end
+
+group group_name do
+  action :modify
+  append true
+  members [user_name, "chadmin"]
+end
+
+[
+	{
+    dir:    "/var/combo",
+    mode:   0775
+  },{
+    dir:    "/var/combo/yui",
+    mode:   0775
+  }
+].each do |data|
+  directory data[:dir] do
+    owner data[:owner] || user_name
+    group data[:group] || group_name
+    mode data[:mode]
+  end
+end
+
+[
+	{
+    dest: "/etc/init/combo.conf",
+    source: "combo.conf",
+    mode: 0644,
+restart: true
+  }
+].each do |data|
+  cookbook_file data[:dest] do
+    source data[:source]
+    owner data[:owner] || "root"
+    group data[:group] || "root"
+    mode data[:mode]
+    notifies "restart", "service[combo]" if data[:restart]
+  end
+end
+
+git "/var/combo" do
+  repo node[:yui_combo][:git_repo]
+  action :checkout
+  user user_name
+  group group_name
+end
+
+service service_name do
+  provider Chef::Provider::Service::Upstart
+  action [ "enable", "start" ]
+end
