ed01994af30eb2c7b7bdc52a52e89eab102371d8
Alex Heiss <aheiss@commercehub.com>
2014-06-27 13:17:19 -0400

    CPE-1928 - Jedis Pooling support

--- environments/mpqa1.rb
+++ environments/mpqa1.rb
@@ -2,7 +2,7 @@ name "mpqa1"
 description "marketplace qa environment 1"
 cookbook "base", "= 0.1.16"
 cookbook "chub-klerk", "= 2.0.0"
-cookbook "chub-buyspace", "= 0.4.2"
+cookbook "chub-buyspace", "= 0.5.0"
 default_attributes "chub-klerk" => {
     "mongo_addresses" => [ "mpqa03:27777", "mpqa05:27777" ],
     "quartz_database_url" => "jdbc:jtds:sqlserver://sqlvm81/mpqa1_klerk_quartz;user=devusr01;password=usrdev",
