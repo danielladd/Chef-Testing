849a07c8502636c30651f05127498c7aeb129397
Jonathan Lazzara <jlazzara@commercehub.com>
2014-06-30 14:07:25 -0400

    update artifactory url with new property

--- environments/albers_prod.rb
+++ environments/albers_prod.rb
@@ -2,7 +2,7 @@ name "albers_prod"
 description "Albers Production Environment"
 default_attributes(
   "chub_albers" => {
-	"jar_url" => ["http://artifactory01.nexus.commercehub.com/artifactory/libs-release-local/com/commercehub/AlbersService/%5BRELEASE%5D/AlbersService-%5BRELEASE%5D-shadow.jar;env.albersprod.current+=true"],
+	"jar_url" => ["http://artifactory01.nexus.commercehub.com/artifactory/libs-release-local/com/commercehub/AlbersService/%5BRELEASE%5D/AlbersService-%5BRELEASE%5D-shadow.jar;env.albers_prod.current+=true"],
     "metrics_environment" => "prod"
   }
 )
