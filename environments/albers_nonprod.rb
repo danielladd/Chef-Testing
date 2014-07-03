586a5bcadd70434df86fe5568c7b15e1e193d22a
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-07-03 11:19:20 -0400

    logstash fixups

--- environments/albers_nonprod.rb
+++ environments/albers_nonprod.rb
@@ -4,5 +4,8 @@ default_attributes(
   "chub_albers" => {
 	"jar_url" => ["http://artifactory01.nexus.commercehub.com/artifactory/libs-release-local/com/commercehub/AlbersService/%5BRELEASE%5D/AlbersService-%5BRELEASE%5D-shadow.jar;env.albers_nonprod.current+=true"],
     "metrics_environment" => "non-prod"
+  },
+  "chub_logstash" => {
+    "env_crt" => "dev"
   }
 )
