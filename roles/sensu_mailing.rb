991e7e750a690dcc7d020182d5e99255eddc004b
chris cucinella <ccucinella@commercehub.com>
2014-07-07 14:21:51 -0400

    update mailing role for Sensu

--- roles/sensu_mailing.rb
+++ roles/sensu_mailing.rb
@@ -7,7 +7,7 @@ default_attributes "chub_sensu" => {
             "pipeline_team" => "pipeline@commercehub.com",
             "scm_team" => "mercurial-admin@commercehub.com",
             "platform_team" => "jlazzara@commercehub.com,mmiller@commercehub.com",
-            "missioncontrol_team" => "InternalEngineering@commercehub.com"
+            "missioncontrol_team" => "BLaviolette@commercehub.com,CMcCormick@commercehub.com,CCucinella@commercehub.com,JLane@commercehub.com,LKulzer@commercehub.com,MBreen@commercehub.com,MBouchard@commercehub.com,SVonDuhn@commercehub.com,ZHarvey@commercehub.com"
         }
     }
 }
