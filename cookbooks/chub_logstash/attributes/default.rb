0981c08e2011817e103e059615d06c6d00b0e581
H "Waldo" Grunenwald <wgrunenwald@commercehub.com>
2014-07-08 09:09:01 -0400

    attempting refactor of chub_logstash cookbook. Needs to be more thoroughly vetted.

--- cookbooks/chub_logstash/attributes/default.rb
+++ cookbooks/chub_logstash/attributes/default.rb
@@ -1,3 +1,4 @@
 default[:chub_logstash][:user]		= "logstash"
+default[:chub_logstash][:version]		= "1.4.1"
 default[:chub_logstash][:base_dir]	= "/opt/logstash"
 default[:chub_logstash][:cert_dir]	= "#{node[:chub_logstash][:base_dir]}/ssl"
