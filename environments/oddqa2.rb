d41b236529bceffcc75cf6311accdd4490a5cbbd
unknown <adistasio@LT-PM-AD.nexus.commercehub.com>
2014-07-11 11:17:15 -0400

    updated slm environments and odd service/rolodex

--- environments/oddqa2.rb
+++ environments/oddqa2.rb
@@ -1,12 +1,27 @@
 name "oddqa2"
-description 'For the ODD QA2 Nodes'
+description 'For the SLM QA2 Nodes'
 cookbook_versions({
+  "chub_apartment"=>"= 0.1.0",
+  "chub_oddrest"=>"= 0.1.0",
+  "chub_rolodex"=>"= 0.1.1",
   "chub_tattler"=>"= 0.3.0"
 })
 default_attributes(
   :slm => {
     :odd_service => {
       :baseUrl => "http://oddqa2-rest.nexus.commercehub.com/"
+    },
+    :apartment => {
+      :baseUrl => "http://oddqa2-apartment.nexus.commercehub.com/"
+    },
+    :rolodex => {
+      :baseUrl => "http://oddqa2-rolodex.nexus.commercehub.com/"
+    },
+    :sso => {
+      :vault => "https://ssoqa1-vault.nexus.commercehub.com:8080/",
+      :census => "https://ssoqa1-census.nexus.commercehub.com:8080/",
+      :castle => "https://ssoqa1-castle.nexus.commercehub.com/",
+      :plaza => "https://ssoqa1-plaza.nexus.commercehub.com/"
     }
   },
   :chub_tattler => {
@@ -15,6 +30,29 @@ default_attributes(
       :name => "tattlerqa2_tat",
       :userName => "devusr01",
       :pass => "usrdev"
+    }
+  },
+  :chub_oddrest => {
+    :db => {
+      :server => "SQLVM03",
+      :name => "oddqa2_odd",
+      :userName => "devusr01",
+      :pass => "usrdev"
+    }
+  },
+  :chub_apartment => {
+    :db => {
+      :server => "SQLVM03",
+      :name => "oddqa2_chub",
+      :userName => "devusr01",
+      :pass => "usrdev"
+    }
+  },
+  :chub_rolodex => {
+    :yui => {
+      :filter => "min",
+      :combine => true,
+      :url => "http://yuicombo1.nexus.commercehub.com:3000/"
     }
   }
 )
