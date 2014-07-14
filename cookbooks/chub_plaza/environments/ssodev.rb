57ac52fe10fe2ed4f81f95a5ce35bdcb33f4e328
kstahl <kstahl@commercehub.com>
2014-07-14 15:06:16 -0400

    verified in vagrant

--- cookbooks/chub_plaza/environments/ssodev.rb
+++ cookbooks/chub_plaza/environments/ssodev.rb
@@ -14,6 +14,9 @@ default_attributes(
     :cas_server_url => "https://ssodev1-castle.nexus.commercehub.com",
     :cas_service_url => "http://plaza.vagrant.dev:8080/secure/portal/shiro-cas",
     :cas_failure_url => "http://plaza.vagrant.dev:8080/secure/portal",
+    :email => {
+      :strategy => "memory"
+    },
     :census_url => "https://ssodev1-census.nexus.commercehub.com:8443",
     :vault_url => "https://ssodev1-vault.nexus.commercehub.com:8443",
     :hazelcast => {
