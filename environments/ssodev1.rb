7fa6960ab061ecb082fa6279f0bfa2966f798537
David M. Carr <dcarr@commercehub.com>
2014-07-18 13:03:20 -0400

    update sso environments

--- environments/ssodev1.rb
+++ environments/ssodev1.rb
@@ -12,7 +12,7 @@ cookbook_versions({
   "chub_castle" => "= 0.1.27",
   "chub_census" => "= 0.1.9",
   "chub_java" => "= 0.1.1",
-  "chub_openldap" => "= 1.12.16",
+  "chub_openldap" => "= 1.12.17",
   "chub_plaza" => "= 0.2.4",
   "chub_sensu_sso" => "= 0.1.17",
   "chub_vault" => "= 0.4.0",
