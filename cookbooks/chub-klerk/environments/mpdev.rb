7ea8852ee47c115e8488e2b4a86cbe4ceb3ee0dc
Matt Hurne <mhurne@commercehub.com>
2014-07-23 13:18:27 -0400

    Tweak environment used by chub-klerk cookbook's Vagrantfile

--- cookbooks/chub-klerk/environments/mpdev.rb
+++ cookbooks/chub-klerk/environments/mpdev.rb
@@ -12,9 +12,10 @@ default_attributes(
         },
         "product_data_application_metadata_update_job_finished_event_endpoint" => "jms:queue:KlerkProductDataApplicationMetadataUpdateJobFinishedEventQueue",
         "dead_product_data_application_metadata_update_job_finished_event_endpoint" => "jms:queue:KlerkProductDataApplicationMetadataUpdateJobFinishedEventDLQ",
-        "jms_provider_url" => "jnp://192.168.33.1:1099"
+        "jms_provider_url" => "jnp://192.168.33.1:1099",
         "sprite_conductor_url" => "http://192.168.33.1:8060/sprite-conductor",
         "sprite_output_container_prefix" => "mpdev-",
+        "java_heap_size" => "256m"
     }
 )
 
