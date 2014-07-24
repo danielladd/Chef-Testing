c0894086163ac75476a23001becc236e3bdf408e
Alex Heiss <aheiss@commercehub.com>
2014-07-24 14:48:14 -0400

    Various updates to prep for production deployment of linux buyspace nodes.

--- cookbooks/chub-buyspace/metadata.rb
+++ cookbooks/chub-buyspace/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aheiss@commercehub.com'
 license          'Apache 2.0'
 description      'Installs/Configures chub-buyspace'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.6.0'
 
 depends "chub_java", "= 0.1.0"
 depends "tomcat", "= 0.15.2"
