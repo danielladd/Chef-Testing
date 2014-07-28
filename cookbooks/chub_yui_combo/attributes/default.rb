adca1bf15b2a300b25aad7f0ea8983c7a8a7f145
Andrew Blick <ablick@commercehub.com>
2014-07-28 14:07:38 -0400

    upped the cookbook version to 0.2.0 for adding a git feature flag attribute and clearing the default value of the git_repo attribute to the empty string

--- cookbooks/chub_yui_combo/attributes/default.rb
+++ cookbooks/chub_yui_combo/attributes/default.rb
@@ -1,9 +1,13 @@
 #Don't enable the default nginx site.
 override[:nginx][:default_site_enabled] = false
 
-#Git Repo containing js/css files to make available to the combo server, as well as the routes.json file.
-default[:yui_combo][:git_repo] = "https://git.nexus.commercehub.com/JDipierro/yui-combo-resources.git"
+#Whether or not to export combo assets from a git repo
+default[:yui_combo][:git_export_enabled] = false
 
+#Git Repo containing assets to make available to the combohandler
+default[:yui_combo][:git_repo] = ""
+
+#The routes that the combohandler will serve
 default[:yui_combo][:routes] = [
 	"yui"
 ]
