e8669289f77eb34bf27a1a2bfc5f417ec804f7c5
Matt Hurne <mhurne@commercehub.com>
2014-07-30 20:25:31 -0400

    Update mpprod1 environment's chub-klerk cookbook version constraint to 3.0.0

--- environments/mpprod1.rb
+++ environments/mpprod1.rb
@@ -1,7 +1,7 @@
 name "mpprod1"
 description "marketplace production environment 1"
 cookbook "base", "= 0.1.16"
-cookbook "chub-klerk", "= 2.0.0"
+cookbook "chub-klerk", "= 3.0.0"
 cookbook "chub-buyspace", "= 0.7.1"
 default_attributes "chub-klerk" => {
     "mongo_addresses" => [ "mdb01:27017", "mdb02:27017", "mdb03:27017" ],
