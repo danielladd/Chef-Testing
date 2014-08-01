6646261bec9f57283715ec3c0370bdfc1e923b37
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-08-01 16:12:08 -0400

    add remove recipe to chub_logsearch_shipper

---
+++ cookbooks/chub_logsearch_shipper/recipes/remove.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: chub_logsearch_shipper
+# Recipe:: remove
+#
+# Copyright 2014, CommerceTechnology, Inc
+#
+# All rights reserved - Do Not Redistribute
+#
+
+directory "c:/Program Files/logsearch-shipper" do
+  action :nothing
+  recursive true
+end
+
+execute 'uninstall_shipper' do
+  cwd 'c:/Program Files/logsearch-shipper'
+  command 'LogsearchShipper.Service.exe uninstall --sudo'
+  only_if do File.exists?("c:/Program Files/logsearch-shipper/LogsearchShipper.Service.exe") end
+  action :nothing
+  notifies :delete, "directory[c:/Program Files/logsearch-shipper]", :immediately
+end
+
+service "logsearch_shipper_net" do
+  supports :restart => true, :reload => true,  :status => true
+  action [:disable, :stop]
+  only_if { File.exists?("c:/Program Files/logsearch-shipper") }
+  notifies :run "execute[uninstall_shipper]",  :immediately 
+end
