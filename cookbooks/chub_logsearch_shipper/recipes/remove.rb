7950613ca37e96db65f79466f28100d3fa38cba5
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-08-01 16:17:15 -0400

    comma

--- cookbooks/chub_logsearch_shipper/recipes/remove.rb
+++ cookbooks/chub_logsearch_shipper/recipes/remove.rb
@@ -24,5 +24,5 @@ service "logsearch_shipper_net" do
   supports :restart => true, :reload => true,  :status => true
   action [:disable, :stop]
   only_if { File.exists?("c:/Program Files/logsearch-shipper") }
-  notifies :run "execute[uninstall_shipper]",  :immediately 
+  notifies :run, "execute[uninstall_shipper]",  :immediately 
 end
