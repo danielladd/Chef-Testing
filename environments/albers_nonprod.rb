e3939436a70c134e593c535dacba6279c11834a5
Jonathan Lazzara <jlazzara@commercehub.com>
2014-08-04 15:30:24 -0400

    update environments

--- environments/albers_nonprod.rb
+++ environments/albers_nonprod.rb
@@ -4,7 +4,8 @@ default_attributes(
   "chub_albers" => {
 	"jar_url" => ["http://artifactory01.nexus.commercehub.com/artifactory/libs-release-local/com/commercehub/AlbersService/%5BRELEASE%5D/AlbersService-%5BRELEASE%5D-shadow.jar;env.albers_nonprod.current+=true"],
     "metrics_environment" => "non-prod",
-    "map_retention" => "24h"
+    "map_retention" => "24h",
+    "dev_repo_url" => "http://hg03:5000/dev"
   },
   "chub_logstash" => {
     "env_crt" => "dev"
--- environments/albers_prod.rb
+++ environments/albers_prod.rb
@@ -4,6 +4,7 @@ default_attributes(
   "chub_albers" => {
 	"jar_url" => ["http://artifactory01.nexus.commercehub.com/artifactory/libs-release-local/com/commercehub/AlbersService/%5BRELEASE%5D/AlbersService-%5BRELEASE%5D-shadow.jar;env.albers_prod.current+=true"],
     "metrics_environment" => "prod",
-    "map_retention" => "288h"
+    "map_retention" => "288h",
+    "dev_repo_url" => "http://hg05:5000/dev"
   }
 )
