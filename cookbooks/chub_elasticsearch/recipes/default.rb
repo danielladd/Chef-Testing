b1722ce4601ec216bab7aecfd9040200e79cba41
David E. Cross <dcross@dcross.laptop.commercehub.com>
2014-08-13 13:46:23 -0400

    add sysctl to elasticsearch config

--- cookbooks/chub_elasticsearch/recipes/default.rb
+++ cookbooks/chub_elasticsearch/recipes/default.rb
@@ -11,3 +11,14 @@ directory "/usr/local/var/data/snapshots" do
   group "elasticsearch"
   mode 00755
 end
+
+execute "sysctl_vm_max_map_count" do
+  command "sysctl -w vm.max_map_count = %{node[:chub_elasticsearch][:vm_max_map_count]}"
+  action :nothing
+end
+
+template "/etc/sysctl.d/90-elasticsearch.conf" do
+  source "elasticsearch.sysctl.erb"
+  variables({vm_max_map_count => "262144"})
+  notifies :run, "execute[sysctl_vm_max_map_count]", :immediately
+end
