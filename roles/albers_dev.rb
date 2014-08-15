9b096d0f243e6faeaf5262faf8a4ff462a46a4fb
unknown <asunkes@PC-QA-AS2.nexus.commercehub.com>
2014-08-15 14:59:27 -0400

    add reboot handler to role

--- roles/albers_dev.rb
+++ roles/albers_dev.rb
@@ -1,3 +1,3 @@
 name "albers_dev"
 description "A windows machine running Albers (in development)"
-run_list "role[albers]", "recipe[chub_windows]", "recipe[chub_albers]", "recipe[chub_logsearch_shipper]"
+run_list "role[albers]", "recipe[chub_windows]", "recipe[chub_albers]", "recipe[chub_logsearch_shipper]", "recipe[windows::reboot_handler]"
