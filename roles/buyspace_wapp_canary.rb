809e49c7dcd71e9bcb60338ba2b0e9a1c4e39799
Dave "Jazz" Jaskolka <djaskolka@commercehub.com>
2014-08-20 16:16:29 -0400

    CPE-1945 - Removed environment-specific configs from canary node.

--- roles/buyspace_wapp_canary.rb
+++ roles/buyspace_wapp_canary.rb
@@ -3,11 +3,7 @@ description "role defining buyspace wapp canary node"
 default_attributes "chub-buyspace" => {
 	"security" => {
 		"shiro" => {
-			"realm" => "sso",
-			"cas" => {
-				"serviceUrl" => "https://mpqa2-buyspace.commercehub.com/shiro-cas",
-				"failureUrl" => "https://mpqa2-buyspace.commercehub.com/auth/fail"
-			}
+			"realm" => "sso"
 		}
 	}
 }
