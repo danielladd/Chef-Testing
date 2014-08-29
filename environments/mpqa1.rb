8847ccf04ce1925fb61561bc51c4ba538fe69e78
Matt Hurne <mhurne@commercehub.com>
2014-08-28 20:31:09 -0400

    Enable klerk logstash appender in mpqa1 environment

--- environments/mpqa1.rb
+++ environments/mpqa1.rb
@@ -1,7 +1,7 @@
 name "mpqa1"
 description "marketplace qa environment 1"
 cookbook "base", "= 0.1.16"
-cookbook "chub-klerk", "= 5.0.0"
+cookbook "chub-klerk", "= 5.1.0"
 cookbook "chub-buyspace", "= 0.10.1"
 default_attributes "chub-klerk" => {
     "mongo_uri" => "mongodb://mpqa03.nexus.commercehub.com:27777,mpqa05.nexus.commercehub.com:27777/?maxPoolSize=50&maxIdleTimeMS=300000",
@@ -18,7 +18,24 @@ default_attributes "chub-klerk" => {
     "sprite_output_container_prefix" => "mpqa1-",
     "sprite_notification_id" => "klerk",
     "sprite_manifest_update_notification_endpoint" => "jms:queue:KlerkSPRITEManifestUpdateNotificationQueue",
-    "jms_provider_url" => "jnp://mpqa03.nexus.commercehub.com:7099"
+    "jms_provider_url" => "jnp://mpqa03.nexus.commercehub.com:7099",
+    "logstash_appender" => {
+        "host" => "mplogs01.nexus.commercehub.com",
+        "key" => "logstash",
+        "threshold" => "INFO",
+        "pool" => {
+            "maxTotal" => "10",
+            "maxIdle" => "5",
+            "minIdle" => "1",
+            "testOnBorrow" => "true",
+            "testOnReturn" => "true",
+            "testWhileIdle" => "true",
+            "blockWhenExhausted" => "false"
+        },
+        "layout" => {
+            "userFields" => "app:klerk,env:mpqa1"
+        }
+    }
 },
 "chub-buyspace" => {
   "mongo" => {
