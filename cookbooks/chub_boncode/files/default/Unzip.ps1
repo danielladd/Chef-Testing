2998a2aa443e2ab042fdea290717529ba5e28c2e
Erik Stegmann <estegmann@commercehub.com>
2014-09-09 11:50:42 -0400

    Chub_boncode cookbook updates

--- cookbooks/chub_boncode/files/default/Unzip.ps1
+++
@@ -1,4 +0,0 @@
-param([string] <%= node[:chub_boncode][:install_location] %>,[string] <%= node[:chub_boncode][:version] %>)
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | out-null
-[System.IO.Compression.ZipFile]::ExtractToDirectory("<%= node[:chub_boncode][:install_location] %>\BonCodeAJP13_<%= node[:chub_boncode][:version] %>.zip","<%= node[:chub_boncode][:install_location] %>\BonCode\")
