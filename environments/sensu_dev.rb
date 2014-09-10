50f0ce9570b995cad88fa444f5935fa44626ab5c
Larry Zarou <larry@zarou.com>
2014-09-10 11:51:05 -0400

    moving around some data from role to env

--- environments/sensu_dev.rb
+++ environments/sensu_dev.rb
@@ -26,6 +26,45 @@ default_attributes(
   chub_log: {
     endpoint: 'lsdev.nexus.commercehub.com',
     endpoint_port: '2352'
+  },
+  graphite: {
+    host: "stats02.nexus.commercehub.com",
+    port: "2003"
+  },
+  chub_sensu: {
+    email: {
+        recipient: "noreply@commercehub.com",
+        subscriptions_mail_to: {
+            pipeline_team: "pipeline@commercehub.com",
+            rhodecode: "mercurial-admin@commercehub.com",
+            platform_team: "jlazzara@commercehub.com,mmiller@commercehub.com",
+            missioncontrol_team: "CCucinella@commercehub.com,LKulzer@commercehub.com",
+            artifactory_prod: "artifactory_admins@commercehub.com",
+            artifactory_alerts: "bcarr@commercehub.com",
+            rundeck_mysql_master: "pipeline@commercehub.com",
+            rundeck_mysql_slave: "pipeline@commercehub.com",
+            apartment: "rolodex@commercehub.com",
+            oddrest: "rolodex@commercehub.com",
+            rolodex: "rolodex@commercehub.com"
+        }
+    },
+    pagerduty: {
+      api_key: "",
+      keys: {
+        apartment: {
+          api_key: "726a0e6e51c045faaf74c5ab8b3e2628"
+        },
+        oddrest: {
+          api_key: "726a0e6e51c045faaf74c5ab8b3e2628"
+        },
+        rolodex: {
+          api_key: "726a0e6e51c045faaf74c5ab8b3e2628"
+        },
+        pipeline_team: {
+          api_key: "0e7c4f5662144f9991def44c23b41146"
+        },
+      }
+    }
   }
 )
 override_attributes(
