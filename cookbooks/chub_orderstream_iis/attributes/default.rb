aea5080c38f83dcb9d949b94ad3c770ad6a6b57e
Erik Stegmann <estegmann@commercehub.com>
2014-09-10 10:00:17 -0400

    Fix typo in chub_orderstream_iis attributes

--- cookbooks/chub_orderstream_iis/attributes/default.rb
+++ cookbooks/chub_orderstream_iis/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:chub_orderstream_iis][:default_app_pool_config][:managed_runtime_version ] = "4.0"
-default[:chub_orderstream_iis][:default_site_root] = "C:/netpub/wwwroot"
+default[:chub_orderstream_iis][:default_site_root] = "C:/inetpub/wwwroot"
 default[:chub_orderstream_iis][:orderstream_site_name] = "OrderStream"
 default[:chub_orderstream_iis][:orderstream_site_root] = "C:/inetpub/orderstream"
 default[:chub_orderstream_iis][:orderstream_site_binding] = "*:443:"
