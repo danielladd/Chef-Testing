86167d440c5f7938087b5fada961b6bd1398b1b4
Erik Stegmann <estegmann@commercehub.com>
2014-09-10 10:01:18 -0400

    swap slashes in paths in chub_orderstream_iis cookbook

--- cookbooks/chub_orderstream_iis/attributes/default.rb
+++ cookbooks/chub_orderstream_iis/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:chub_orderstream_iis][:default_app_pool_config][:managed_runtime_version ] = "4.0"
-default[:chub_orderstream_iis][:default_site_root] = "C:/inetpub/wwwroot"
+default[:chub_orderstream_iis][:default_site_root] = "C:\\inetpub\\wwwroot"
 default[:chub_orderstream_iis][:orderstream_site_name] = "OrderStream"
-default[:chub_orderstream_iis][:orderstream_site_root] = "C:/inetpub/orderstream"
+default[:chub_orderstream_iis][:orderstream_site_root] = "C:\\inetpub\\orderstream"
 default[:chub_orderstream_iis][:orderstream_site_binding] = "*:443:"
 default[:chub_orderstream_iis][:orderstream_site_ssl_enabled] = "true"
 default[:chub_orderstream_iis][:orderstream_site_protocol] = :https
